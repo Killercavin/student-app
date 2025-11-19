49e922bf7eecc2ce9dfee2d88bb9a5ae554f48db
Cavin <cavinlarry001@gmail.com>
2025-11-20 00:26:02 +0300

    feat: Setup security configuration

--- src/main/java/com/stephen/studentapp/config/SecurityConfig.java
+++ src/main/java/com/stephen/studentapp/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.stephen.studentapp.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
@@ -14,5 +18,15 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(h)
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)  throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(
+                        authorizeRequests ->
+                                authorizeRequests
+                                        .requestMatchers("/api/v1/auth/**").permitAll()
+                                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin.loginPage("/api/v1/auth/login").permitAll());
+        return httpSecurity.build();
+    }
 }
